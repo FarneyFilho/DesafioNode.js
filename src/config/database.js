dfcabf6933203ca15e5672e130f7445cde39a87f
FarneyFilho <farneydevweb@outlook.com>
2022-03-03 14:47:13 -0300

    Inicio do Projeto

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud-node',
+    define: {
+        timestamp: true,
+        underscored: true,
+    }
+}
